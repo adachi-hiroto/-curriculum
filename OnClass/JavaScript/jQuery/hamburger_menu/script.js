e6078de6e08aabab73ce0963c257fdc2de2be899
adachi-hiroto <hi818roto@icloud.com>
2023-12-23 21:53:01 +0900

    commit

--- OnClass/JavaScript/jQuery/hamburger_menu/script.js
+++
@@ -1,7 +0,0 @@
-const ham = $('#js-hamburger');
-const nav = $('#js-nav');
-ham.on('click', function () { //ハンバーガーメニューをクリックしたら
-  ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
-  nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
-
-});
